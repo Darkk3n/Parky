e5777aac8c6f1711eeb659514fbf11288dc653fd
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-10-12 10:17:26 -0500

    Implemented Patch method

--- Controllers/NationalParksController.cs
+++ Controllers/NationalParksController.cs
@@ -54,10 +54,26 @@ namespace Parky.Controllers
 
             if (!parkRepo.CreateNationalPark(parkToCreate))
             {
-                ModelState.AddModelError(string.Empty, $"An error ocurred when create Park: {parkDto.Name}");
+                ModelState.AddModelError(string.Empty, $"An error ocurred when creating Park: {parkDto.Name}");
                 return StatusCode(StatusCodes.Status500InternalServerError, ModelState);
             }
             return CreatedAtRoute("GetNationalPark", new { parkId = parkToCreate.Id }, parkToCreate);
+        }
+
+        [HttpPatch("{parkId:int}", Name = "UpdateNationalPark")]
+        public IActionResult UpdateNationalPark(int parkId, [FromBody] NationalParkDto parkDto)
+        {
+            if (parkDto == null || parkId != parkDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+            var parkToUpdate = mapper.Map<NationalPark>(parkDto);
+            if (!parkRepo.UpdateNationalPark(parkToUpdate))
+            {
+                ModelState.AddModelError(string.Empty, $"An error ocurred when updating Park: {parkDto.Name}");
+                return StatusCode(StatusCodes.Status500InternalServerError, ModelState);
+            }
+            return NoContent();
         }
     }
 }
